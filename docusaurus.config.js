ae5368f0b3b1139163ed36adb48dda67a45f1d22
zoetanghk852 <zoetang852@gmail.com>
2024-07-08 18:18:48 +0800

    config🔧: try deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
   url: `https://${organizationName}.github.io`,
-  baseUrl: `/${projectName}/`,
+  baseUrl: `/`,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
   favicon: "img/favicon.ico",
